d1205210f3b923c6da58b901679b97a38d96461e
Nipesh <nipeshincbca2021@gmail.com>
2026-03-26 21:50:36 +0545

    register validation commit

--- blog_app/views/auth_view.py
+++ blog_app/views/auth_view.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 
+def register_method(request):
+    errors = {}
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        # validation
+        if not username:
+            errors['username'] = "Username is required"
+        
+        if not email:
+            errors['email'] = "Email is required"
+
+        if not password:
+            errors['password'] = "Password enter gara"
+        
+        if not confirm_password:
+            errors['confirm_password'] = "Confirm password is required"
+        
+        if errors:
+            return render(request,'auth/register.html',{'error':errors,'prev':request.POST})
+
+
+    return render(request,'auth/register.html')
+
+
 def login_method(request):
     return render(request,'auth/login.html')
 
-def register_method(request):
-    return render(request,'auth/register.html')

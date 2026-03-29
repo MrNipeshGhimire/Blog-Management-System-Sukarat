0539f16dcda408e2931e280656b10bed7fc1f9f3
Nipesh <nipeshincbca2021@gmail.com>
2026-03-29 20:48:14 +0545

    register and login part completed

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views.main_view import index
-from .views.auth_view import login_method, register_method
+from .views.auth_view import login_method, register_method,logout_method
 
 urlpatterns = [
     path('',index , name="index"),
     path('login/',login_method , name="login"),
-    path('register/',register_method, name="register")
+    path('register/',register_method, name="register"),
+    path('logout/',logout_method, name="logout")
 ]
--- blog_app/views/auth_view.py
+++ blog_app/views/auth_view.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+
 
 def register_method(request):
     errors = {}
@@ -11,9 +15,14 @@ def register_method(request):
         # validation
         if not username:
             errors['username'] = "Username is required"
+        if User.objects.filter(username=username).exists():
+            errors['username'] = "Username already exists"
+
         
         if not email:
             errors['email'] = "Email is required"
+        elif User.objects.filter(email=email).exists():
+            errors['email'] = "Email already exists !! use another"
 
         if not password:
             errors['password'] = "Password enter gara"
@@ -21,13 +30,59 @@ def register_method(request):
         if not confirm_password:
             errors['confirm_password'] = "Confirm password is required"
         
+        if password != confirm_password:
+            errors['confirm_password'] = "Confirm password is not matched !!"
+        
         if errors:
             return render(request,'auth/register.html',{'error':errors,'prev':request.POST})
-
-
+        else:
+            user = User.objects.create_user(
+                username=username,
+                email= email,
+                password=password
+            )
+            user.save()
+            messages.success(request,"User registered successfully")
+            return redirect('login')
+            
     return render(request,'auth/register.html')
 
 
 def login_method(request):
+    errors = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+       # validation 
+        if not username:
+           errors['username'] = "Username is required"
+        elif not User.objects.filter(username=username).exists():
+            errors['username'] = "Username does not exists"
+
+        if not password:
+           errors['password'] = "Password is required"
+        
+        if errors:
+           return render(request,'auth/login.html',{'error':errors,'data':request.POST})
+        else:
+            # if there is not any errors
+            # authenticate() => username and password check graxa 
+            user = authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)  # session generate garxa 
+                messages.success(request,"User logged in successfully !! ")
+                return redirect('index')
+            else:
+                messages.error(request,'Incorrect Password')
+                return redirect('login')
+                
     return render(request,'auth/login.html')
+
+
+def logout_method(request):
+    logout(request)
+    messages.success(request,'Logout Successfully !!')
+    return redirect('index')
+
 
